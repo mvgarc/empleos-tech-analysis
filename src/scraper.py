fd376a85b4f656886729333b0a912aced874849c
María García <mariav2601@gmail.com>
2025-04-28 12:58:59 -0400

    Agregar función scraper_page y mejorar formato del archivo scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -5,6 +5,10 @@ import time
 
 
 BASE_URL = "https://www.tecnoempleo.com/ofertas-trabajo/?te=data+analyst&pr=231"
+
+#Simulación de un navegador web para evitar bloqueos por parte del servidor
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
+
+def scraper_page(url):
